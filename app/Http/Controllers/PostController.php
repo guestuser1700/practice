90f7e8aa34473a4adf8e5d15c4046517fa6a0fea
guestuser1700 <daisuke_0902@icloud.com>
2022-10-07 09:53:41 +0000

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+use App\Post;
+use App\Http\Request\PostRequest;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);  
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post'=>$post]);
+    }
+    public function create()
+    {
+        return view('posts/create');
+    }
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post'=>$post]);
+    }
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
+}
+
+}
