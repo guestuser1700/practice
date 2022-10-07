4c269b92dfedb9e9e118756d3e301479454adde8
guestuser1700 <daisuke_0902@icloud.com>
2022-10-07 17:37:35 +0000

    編集画面の追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,52 +1,3 @@
-Loading history...
-
-
-野田大介
-  17:25
-わかりました
-
-
-渡邊頌（メンター）
-  17:25
-よろしくお願いします
-17:25
-再起動したら下記のリンクから入ってきて下さい
-https://us05web.zoom.us/j/9342042879?pwd=N1NHclNIWTZkY0swYUZreEhsekNoUT09
-
-
-渡邊頌（メンター）
-  17:00
-今日の面談はこちらでやります！
-https://us05web.zoom.us/j/9342042879?pwd=N1NHclNIWTZkY0swYUZreEhsekNoUT09
-
-
-渡邊頌（メンター）
-  17:22
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body }}</p>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
-    </body>
-</html>
-New
-17:24
 
 <!DOCTYPE HTML>
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
@@ -71,5 +22,6 @@ New
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <p class="edit">[<a href="/posts/{{$post->id}}/edit">edit</a>]</p>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/', 'PostController@index');
-Route::get('/posts', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}','PostController@show');
-Route::get('/posts','PostController@store');
+Route::post('/posts','PostController@store');
+Route::get('/posts/{post}/edit','PostController@edit');
+Route::put('/posts/{post}','PostController@update');
